c2330fcf506f1165b92dd02103751f958b4712d6
NeSustat <nesustat0v@icloud.com>
2026-03-09 00:10:27 +0300

    lab2

--- lab_2/task_3.cpp
+++ lab_2/task_3.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <iomanip>
 
+// МПИ
+
 int k = 0;
 const double eps = 1E-3;
 
@@ -10,17 +12,27 @@ double Xk1[4] = {0, 0, 0, 0};
 
 double Xk2[4] = {0, 0, 0, 0};
 
-const double a[4][4] = {{5.554, 0.252, 0.496, 0.237}, 
+double a1[4][4] = {{5.554, 0.252, 0.496, 0.237}, 
                         {0.580, 4.953, 0.467, 0.028}, 
                         {0.319, 0.372, 8.935, 0.520}, 
                         {0.043, 0.459, 0.319, 4.778}};
 
-const double b[4] = {0.442, 0.464, 0.979, 0.126};
+double a[4][4];
+
+double b1[4] = {0.442, 0.464, 0.979, 0.126};
+
+double b[4] = {0, 0, 0, 0};
 
 double edMatrix[4][4] = {{1, 0, 0, 0},
                         {0, 1, 0, 0},
                         {0, 0, 1, 0},
                         {0, 0, 0, 1}};
+
+
+double H[4][4] = {{0.1, 0, 0, 0},
+                {0, 0.1, 0, 0},
+                {0, 0, 0.1, 0},
+                {0, 0, 0, 0.1}};
 
 // x1^(k+1) = b[0][0] * x1^(k) + b[1][0] * x2^(k) + ... + b[4][0] * x4^(k) + b[0]
 
@@ -67,6 +79,16 @@ void checkRes(){
 }
 
 int main(){
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            double count = 0;
+            for (int k = 0; k < 4; k++){
+                count += a1[i][k] * H[k][j];
+            }
+            a[i][j] = count;
+            b[i] += H[i][j] * b1[j];
+        }
+    }
     for (int i = 0; i < 4; i ++){
         for (int j = 0; j < 4; j++){
             edMatrix[i][j] -= a[i][j];
@@ -84,4 +106,8 @@ int main(){
     } while (checkEnd() > eps);
     std::cout << std::fixed << std::setprecision(4);
     checkRes();
+    std::cout << "\n\nb: ";
+    for (double k : b){
+        std::cout << k << " ";
+    }
 }
