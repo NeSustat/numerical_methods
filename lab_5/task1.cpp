4232ab58db7b0ca69ff91c7acbff2c46547588ea
NeSustat <nesustat0v@icloud.com>
2026-04-06 23:24:37 +0300

    lab5

--- lab_5/task1.cpp
+++ lab_5/task1.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
-/*
-x | x0 x1 x2 x3
-y | y0 y1 y2 y3
+const int N = 5;
+double X[N] = {0.351, 0.867, 1.315, 2.013, 2.859};
+double Y[N] = {0.605, 0.218, 0.205, 1.157, 5.092};
 
-y(x) = L(x) = Sigma(l(x)_i)
-l(x)i = ((x - x_j)(x - x_j)(x - x_j) / (x_i - x_j)(x_i - x_j)(x_i - x_j)) j≠i
-
-Ньютон
-N(x) = y_0 + (y_1 - y_0 / x_1 - x_0)(x - x_0) + (y)
-*/
-
-double X[5] = {0.351, 0.867, 1.315, 2.013, 2.859};
-double Y[5] = {0.605, 0.218, 0.205, 1.157, 5.092};
-
-double l(int i, double x){
+// Лагранж
+double l(int i, double x) {
     double a = 1, b = 1;
-    for (int j = 0; j < 5; j++){
-        if (i != j){
+    for (int j = 0; j < N; j++) {
+        if (i != j) {
             a *= x - X[j];
             b *= X[i] - X[j];
         }
@@ -26,17 +18,41 @@ double l(int i, double x){
     return (a * Y[i]) / b;
 }
 
-double L(double x){
+double L(double x) {
     double a = 0;
-    for (int i = 0; i < 5; i++){
-        a += l(i, x);
-    }
+    for (int i = 0; i < N; i++) a += l(i, x);
     return a;
 }
 
+// Ньютон (разделённые разности)
+double dd[N][N]; // divided differences table
 
+void buildNewton() {
+    for (int i = 0; i < N; i++) dd[i][0] = Y[i];
+    for (int j = 1; j < N; j++)
+        for (int i = 0; i < N - j; i++)
+            dd[i][j] = (dd[i+1][j-1] - dd[i][j-1]) / (X[i+j] - X[i]);
+}
 
-int main(){
-    std::cout << "Lagransh\nL(x1 + x2) = " << L(X[0] + X[1]) << std::endl;
+double Newton(double x) {
+    double result = dd[0][0];
+    double term = 1;
+    for (int i = 1; i < N; i++) {
+        term *= x - X[i-1];
+        result += dd[0][i] * term;
+    }
+    return result;
+}
 
+int main() {
+    buildNewton();
+    double x = X[0] + X[1]; // 0.351 + 0.867 = 1.218
+
+    std::cout << std::fixed << std::setprecision(6);
+    std::cout << "========================================\n";
+    std::cout << "  Интерполяция в точке x = " << x << "\n";
+    std::cout << "========================================\n";
+    std::cout << "  Лагранж  L(x) = " << L(x)      << "\n";
+    std::cout << "  Ньютон   N(x) = " << Newton(x) << "\n";
+    std::cout << "========================================\n";
 }
